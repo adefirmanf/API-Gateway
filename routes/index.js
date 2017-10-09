495727d9775cbd1528b538790e52ed57cabd7e67
adefirmanf <syskeys32@gmail.com>
2017-10-09 18:21:02 +0700

    Add sample routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var Router = require('express').Router()
 var proxy = require('express-http-proxy')
+var json = require('../routes.json')
+var _ = require('lodash')
 
-Router.get('/', function(req, res, next){
-	res.json("Hello world")
+Router.get('/:routePath', function(req, res, next){
+	var path = _.find(json.routes, {'path' : `/${req.params.routePath}`})
+	
+	if (typeof path == 'undefined'){
+		res.json({err : {"code" : 404, "message" : "Not found"}})
+		return
+	}
+	res.json(path.message)
 })
+
 
 module.exports = Router
