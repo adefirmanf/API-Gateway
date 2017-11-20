2b7c0eef14a477f2655cf3d0d4fe3f869e397fbc
adefirmanf <syskeys32@gmail.com>
2017-11-20 17:45:23 +0700

    Using proxy to target

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,18 @@ const Router = require('express').Router()
 const proxy = require('express-http-proxy')
 const client = require('../lib/api')
 
-client().then((msg)=>{
-	msg.forEach(function(key){
+const opt = {
+	proxyReqOptDecorator : function(proxyReqOpts, srcReq){
+		proxyReqOpts.headers['Content-Type'] = 'application/json';
+		return proxyReqOpts;
+	}
+}
+
+client().then((src)=>{
+	src.forEach(function(key){
 		let url = key._source.url;
 		let target = key._source.target
-			Router.use(url, (req, res)=>{
-				res.json(target)
-			})
+		Router.use(url, proxy(target, opt))
 	})
 }).catch((err)=>{
 	console.log(err)
